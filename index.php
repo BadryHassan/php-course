3f5b4f75614f993d960d1a02ea04faeddfd7854b
BadryHassan <68699881+BadryHassan@users.noreply.github.com>
2020-07-31 19:00:08 +0300

    Tax Calculator Function

--- index.php
+++ index.php
@@ -36,6 +36,15 @@
     $tax= 0.0825;
     $addedTax= $amount*$tax;
     echo $addedTax;
+    
+    //****Tax Calculator****
+    function tax_calc($amount,$tax){
+      $calculate_tax =$amount*$tax;
+      $amount=round($amount+$calculate_tax,2);
+      return $amount;
+    }
+
+    echo tax_calc(750,0.223);
 
     ?>
   </body>
