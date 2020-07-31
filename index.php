8cc1f2c66bd80139d60220698aee509627bfcd55
BadryHassan <68699881+BadryHassan@users.noreply.github.com>
2020-07-31 18:04:24 +0300

    Adding Variable Array and dispaying the results

--- index.php
+++ index.php
@@ -7,12 +7,18 @@
     <?php
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
+    //*****Variables******
     $name= 'PHP Store';
     $credit=1000;
+    $products['Computer'] = 750;
+    $products['Car'] = 15000;
+    $products['iPhone'] = 1000;
+    $products['Toaster'] = 75;
+
+    //******DisplayResults******
     echo "<h1>Welcome to ".$name."!</h1>";
-    echo "<h2>You Have ".$credit." in your wallet </h2>";
-
-
+    echo "<h2>You Have $".$credit." in your wallet </h2>";
+    echo "<p>A Car cost $".$products['Car']."</p>";
 
     ?>
   </body>
